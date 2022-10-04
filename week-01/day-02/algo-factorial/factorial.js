bbbe04eff666603f62316955d8f784428481ee44
Carter Davis <carterldavis2002@gmail.com>
2022-10-04 16:44:47 -0500

    factorial.js working iterative solution

--- week-01/day-02/algo-factorial/factorial.js
+++ week-01/day-02/algo-factorial/factorial.js
@@ -1,3 +1,16 @@
+//Iterative factorial solution
+exports.factorialIterative = function(inputNumber) {
+    let result = 1n; //Holds final result of factorial
+
+    //Multiply result by current inputNumber val until inputNumber <= 0
+    while (inputNumber > 1) {
+        result *= BigInt(inputNumber);
+        inputNumber--;
+    }
+
+    return parseInt(result.toString(), 10);
+}
+
 //Recursive factorial solution
 exports.factorialRecursive = factorialRecursive;
 function factorialRecursive(inputNumber) {
