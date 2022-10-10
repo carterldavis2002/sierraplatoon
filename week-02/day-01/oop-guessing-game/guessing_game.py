42499325f9afc3268e469f9c9f069cbfebfa82a2
Carter Davis <carterldavis2002@gmail.com>
2022-10-10 12:45:01 -0500

    Documentation for guessing_game.py

--- week-02/day-01/oop-guessing-game/guessing_game.py
+++ week-02/day-01/oop-guessing-game/guessing_game.py
@@ -1,9 +1,19 @@
+#GuessingGame class simulates a simple guessing game
 class GuessingGame:
 
+    #Initialize correct answer to passed answer
+
+    #Set last guess to a number that is not answer so
+    #the solved instance method will not return true before
+    #a guess is made
     def __init__(self, answer):
         self.answer = answer
         self.last_guess = answer - 1
 
+    #Convert entered guess to an integer and set it to last guess
+
+    #Return appropriate statement based on how the guess compares
+    #to the answer
     def guess(self, user_guess):
         user_guess = int(user_guess)
         self.last_guess = user_guess
@@ -14,5 +24,6 @@ class GuessingGame:
         else:
             return "correct"
 
+    #Game is solved if the previous guess was the answer
     def solved(self):
         return self.last_guess == self.answer
